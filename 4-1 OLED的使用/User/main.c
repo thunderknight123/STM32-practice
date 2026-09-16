682871a5ac90142a3f022363bee0688b620324c4
ThunderKnight <a3057478818@163.com>
2026-09-16 21:34:02 +0800

    Add OLED display project (4-1)

---
+++ 4-1 OLED的使用/User/main.c
@@ -0,0 +1,16 @@
+#include "stm32f10x.h"                  // Device header
+#include "Delay.h"
+#include "OLED.h"
+
+
+int main(void)
+{
+	OLED_Init();
+
+	OLED_ShowString(1 , 1 , "Hello World");
+	while(1)
+	{
+		;
+	}
+}
+
